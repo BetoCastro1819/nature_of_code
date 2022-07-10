95d3eef503985f269cfff274a3f43669fd6057d8
Beto Castro <betocastro1819@gmail.com>
2022-07-09 23:58:20 -0300

    Random walker that tends to follow mouse position

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+// int randomIndex = rand() % MAX; -> returns a value between 0 and MAX (not inclusive) 
+
+// NOTE: Continue reading from "1.4: A normal distribution  of random numbers"
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(960, 960), "SFML works!");
+
+    srand(time(NULL));
+
+	sf::RectangleShape walker(sf::Vector2f(1, 1));
+	walker.setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2));
+	walker.setFillColor(sf::Color::White);
 
     while (window.isOpen())
 	{
@@ -17,9 +29,46 @@ int main()
 			}
         }
 
-        window.clear();
-        window.draw(shape);
+        // window.clear(sf::Color::White);
+
+		float deltaX = 0.0f;
+		float deltaY = 0.0f;
+
+		float probability = (float)(rand() % 101) / 100.0f;
+
+
+		if (probability < 0.3)
+		{
+			sf::Vector2i mousePos = sf::Mouse::getPosition(window); 
+			sf::Vector2i walkerPos((int)walker.getPosition().x, (int)walker.getPosition().y);
+			int mouseRelPosX = mousePos.x - walkerPos.x;
+			int mouseRelPosY = walkerPos.y - mousePos.y;
+
+			deltaX = (mouseRelPosX > 0) ? 1 : -1;
+			deltaY = (mouseRelPosY > 0) ? -1 : 1; // negative Y means "up"
+		}
+		else if (probability > 0.5 && probability < 0.6)
+		{
+			deltaX = -1;
+		}
+		else if (probability > 0.7 && probability < 0.8)
+		{
+			deltaY = 1;
+		}
+		else if (probability > 0.8 && probability < 0.9)
+		{
+			deltaY = -1;
+		}
+		else if (probability > 0.9 && probability < 1.0)
+		{
+			deltaX = 1;
+		}
+
+		walker.move(deltaX, deltaY);
+
+        window.draw(walker);
         window.display();
+
     }
 
     return 0;
